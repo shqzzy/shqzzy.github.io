757ceba16d4a5bed283544d11bd0fd2c005fa9bd
shqzzy <shqlsl@sina.com>
2026-05-29 17:02:47 +0800

    delete files
    delete files

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-# requires you hold the private key of the pi in question :)
-
-nix build
-scp -r ./result/* pi@hugopeters.me:/var/www/blog
--- main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from src import cli
-
-if __name__ == "__main__":
-    cli.main()
